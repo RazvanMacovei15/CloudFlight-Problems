291491a8a4cda93c90386666100c00aa1e47387c
RazvanMacovei15 <razvanmc15@gmail.com>
2023-05-16 22:02:29 +0300

    final auction bidding

--- AuctionBiding/AuctionBiding/src/AuctionBidding.java
+++ AuctionBiding/AuctionBiding/src/AuctionBidding.java
@@ -4,48 +4,185 @@ public class AuctionBidding {
 
     private String[] gameInput;
 
+
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        Set<String> biddersList = new HashSet<>();
+        List<String> biddingHistory = new ArrayList<>();
 
-
+        List<String> biddingHistoryBuyNow = new ArrayList<>();
 
         System.out.print("Enter integers and strings separated by commas: ");
-//        String[] tokens = scanner.nextLine().split(",");
+
         String input = scanner.nextLine();
+
         List<String> bids = Arrays.asList(input.split(","));
 
         int highestBid = 0;
         String highestBidder = "";
-        int start1 = Integer.parseInt(bids.get(0));
+
         int startingPrice = Integer.parseInt(bids.get(0));
+        int buyNowPrice = Integer.parseInt(bids.get(1));
+
         int newPrice = startingPrice;
-        for(int i = 1; i<bids.size();i+=2){
-            String currentBidder = bids.get(i);
-            biddersList.add(currentBidder);
-            int currentBid = Integer.parseInt(bids.get(i+1));
 
-            if(currentBid == startingPrice){
-                highestBidder = currentBidder;
-                newPrice = startingPrice;
-            }
-            else if (currentBid > highestBid){
-                highestBidder = currentBidder;
-                highestBid = currentBid;
-                newPrice = Integer.parseInt(bids.get(i-1)) + 1;
-            } else if (currentBid < highestBid) {
-                newPrice = currentBid + 1;
-            } else if(currentBid == highestBid){
-                newPrice = currentBid;
-            }
-            if(biddersList.size()==1){
-                highestBidder = currentBidder;
-                newPrice = start1;
-            }
+        biddingHistory.add("-");
+        biddingHistory.add(String.valueOf(startingPrice));
+
+        String firstBidder = bids.get(2);
+        int firstBid = Integer.parseInt(bids.get(3));
+
+        biddingHistory.add(firstBidder);
+        biddingHistory.add(String.valueOf(newPrice));
+        boolean exit = false;
+
+        if(buyNowPrice == 0){
+            do{
+                if(bids.size() == 4 ){
+                    highestBidder = firstBidder;
+                    highestBid = firstBid;
+                }else{
+                    highestBidder = firstBidder;
+                    highestBid = firstBid;
+                    for(int i = 4; i<bids.size(); i+=2){
+                        String currentBidder = bids.get(i);
+                        int currentBid = Integer.parseInt(bids.get(i+1));
+                        if(currentBidder.equals(highestBidder)){
+                            if(currentBid < highestBid){
+                                newPrice = currentBid + 1;
+                                biddingHistory.add(currentBidder);
+                                biddingHistory.add(String.valueOf(newPrice));
+
+                            }else if(currentBid == highestBid){
+                                biddingHistory.add(currentBidder);
+                                biddingHistory.add(String.valueOf(newPrice));
+
+                            }else{
+                                highestBid = currentBid;
+//                                biddingHistory.add(currentBidder);
+//
+//                                biddingHistory.add(String.valueOf(newPrice));
 
 
+                            }
+                        }else {
+                            if(currentBid > highestBid){
+                                newPrice = highestBid + 1;
+                                highestBidder = currentBidder;
+                                highestBid = currentBid;
+                                biddingHistory.add(currentBidder);
+                                biddingHistory.add(String.valueOf(newPrice));
+
+                            } else if(currentBid == highestBid){
+                                newPrice = highestBid;
+                                biddingHistory.add(highestBidder);
+
+                                biddingHistory.add(String.valueOf(newPrice));
+
+                            } else {
+                                newPrice = currentBid + 1;
+                                biddingHistory.add(highestBidder);
+                                biddingHistory.add(String.valueOf(newPrice));
+
+                            }
+                        }
+                    }
+                }
+
+//            System.out.println(highestBidder + "," + newPrice);
+                String result = String.join(",", biddingHistory);
+                System.out.println(result);
+
+                if(newPrice <= buyNowPrice){
+                    exit = true;
+                }
+
+            }while(!exit && newPrice < buyNowPrice);
+        }else{
+            do{
+                if(bids.size() == 4 ){
+                    highestBidder = firstBidder;
+                    highestBid = firstBid;
+                }else{
+                    highestBidder = firstBidder;
+                    highestBid = firstBid;
+                    for(int i = 4; i<bids.size(); i+=2){
+                        String currentBidder = bids.get(i);
+                        int currentBid = Integer.parseInt(bids.get(i+1));
+                        if(currentBidder.equals(highestBidder)){
+                            if(currentBid < highestBid){
+                                newPrice = currentBid + 1;
+                                biddingHistory.add(currentBidder);
+                                if(newPrice > buyNowPrice){
+                                    biddingHistory.add(String.valueOf(buyNowPrice));
+                                }else{
+                                    biddingHistory.add(String.valueOf(newPrice));
+                                }
+                            }else if(currentBid == highestBid){
+                                biddingHistory.add(currentBidder);
+                                if(newPrice > buyNowPrice){
+                                    biddingHistory.add(String.valueOf(buyNowPrice));
+                                }else{
+                                    biddingHistory.add(String.valueOf(newPrice));
+                                }
+                            }else{
+                                highestBid = currentBid;
+//                                biddingHistory.add(currentBidder);
+//                                if(newPrice > buyNowPrice){
+//                                    biddingHistory.add(String.valueOf(buyNowPrice));
+//                                }else{
+//                                    biddingHistory.add(String.valueOf(newPrice));
+//                                }
+
+                            }
+                        }else {
+                            if(currentBid > highestBid){
+                                newPrice = highestBid + 1;
+                                highestBidder = currentBidder;
+                                highestBid = currentBid;
+                                biddingHistory.add(currentBidder);
+                                if(newPrice > buyNowPrice){
+                                    biddingHistory.add(String.valueOf(buyNowPrice));
+                                }else{
+                                    biddingHistory.add(String.valueOf(newPrice));
+                                }
+                            } else if(currentBid == highestBid){
+                                newPrice = highestBid;
+                                biddingHistory.add(highestBidder);
+                                if(newPrice > buyNowPrice){
+                                    biddingHistory.add(String.valueOf(buyNowPrice));
+                                }else{
+                                    biddingHistory.add(String.valueOf(newPrice));
+                                }
+                            } else {
+                                newPrice = currentBid + 1;
+                                biddingHistory.add(highestBidder);
+                                if(newPrice > buyNowPrice){
+                                    biddingHistory.add(String.valueOf(buyNowPrice));
+                                }else{
+                                    biddingHistory.add(String.valueOf(newPrice));
+                                }
+                            }
+                        }
+                    }
+                }
+
+//            System.out.println(highestBidder + "," + newPrice);
+                String result = String.join(",", biddingHistory);
+                System.out.println(result);
+
+                if(newPrice <= buyNowPrice){
+                    exit = true;
+                }
+
+            }while(!exit && newPrice < buyNowPrice);
         }
-        System.out.println(highestBidder + ","+newPrice);
+
+
+
+//        if (newPrice < buyNowPrice) {
+//            System.out.println("Auction ended without reaching the buy now price.");
+//        }
     }
 }
